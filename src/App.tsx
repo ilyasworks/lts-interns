9a78396efe7bd0b30e44ef36b7ce084b0da941c1
Fatimahadi786 <fatimahadi151214@gmail.com>
2024-07-02 16:12:30 +0500

    home 3 page complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/home"
+import Homecard from "./Components/Home/homecard";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}>
-          
-        </Route>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/*" element={<Homecard/>} ></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/Components/Home/home.tsx
+++ src/Components/Home/home.tsx
@@ -2,6 +2,9 @@ import './Home.css';
 import  BG1 from '../../assets/Background1.png';
 import BG2 from '../../assets/Background2.png';
 import Navbar from '../navbar';
+import Homecard from './homecard';
+import TravelEasy from './TravelEasy';
+
 
 const Home = () => {
   return (
@@ -12,8 +15,11 @@ const Home = () => {
       <img className="bg-image1" src={BG1} alt="Background 1" />
       <img className="bg-image2" src={BG2} alt="Background 2" />
     </div>
-   
+
+    <Homecard/>
+    <TravelEasy/>
    </>
+
   );
 };
 
